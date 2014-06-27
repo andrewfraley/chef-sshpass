6d2a4b9be689fdaa2677e0caf7eec0a8ea9898d0
Andy Fraley <andrew.fraley@gmail.com>
2014-06-26 21:04:26 -0400

    fixed checksum

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,6 @@
 ark "sshpass" do
    url node['sshpass']['url']
-   extension "tar.gz"
    version node['sshpass']['version']
-   checksum "c3f78752a68a0c3f62efb3332cceea0c8a1f04f7cf6b46e00ec0c3000bc8483e"
+   checksum node['sshpass']['checksum']
    action [ :install_with_make ]
  end
